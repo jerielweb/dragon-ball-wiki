e272db6daaf4e22c731cf34c9b5742ac6f1435a7
Jeriel Beckford <jerielweb@outlook.es>
2025-11-30 20:24:00 -0600

    Actualizar page.tsx

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -100,7 +100,7 @@ export default async function CharacterPage({
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {character.transformations.map((transformation) => (
                 <div
-                  key={transformation.id}
+                  key={transformation.name}
                   className="border border-amber-600 rounded-lg p-4 flex flex-col items-center"
                 >
                   <Image
